9ae9ce58fa7bca353f643f2c86983d77e3ba896f
Santiagooooo <1242255968@qq.com>
2021-04-21 22:20:44 +0800

    first commit

---
+++ src/com/santiago/Main.java
@@ -0,0 +1,13 @@
+package com.santiago;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(1,1);
+        System.out.println("hello world");
+    }
+}
